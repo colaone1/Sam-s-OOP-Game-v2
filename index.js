2b37d617d816773645f7cf6aad15ed909220699a
N Bizzell <bizzbizzell@gmail.com>
2020-07-03 23:18:16 +0100

    step back abstraction not required

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-import {gameSetup} from './modules/setup.js'
+import {Room} from './modules/room.js'
+import {Item} from './modules/item.js'
+import {Enemy} from './modules/character.js'
+
+
+//create the indiviual room objects and add their descriptions
+const Kitchen = new Room("kitchen");
+Kitchen.description = "a long narrow room with worktops on either side and a large bench in the middle";
+const Lounge = new Room("lounge");
+Lounge.description = "a large room with two sofas and a large fire place";
+const GamesRoom = new Room("Games Room");
+GamesRoom.description = "a large room with a pool table at it's centre";
+const Hall = new Room("hall");
+Hall.description = "a grand entrance hall with large paintings around the walls";
+
+//link the rooms together
+Kitchen.linkRoom("south", Lounge);
+Kitchen.linkRoom("east", Hall);
+Lounge.linkRoom("north", Kitchen);
+Lounge.linkRoom("east", GamesRoom);
+GamesRoom.linkRoom("west", Lounge);
+GamesRoom.linkRoom("north", Hall);
+Hall.linkRoom("south", GamesRoom);
+Hall.linkRoom("west", Kitchen);
+
+//add characters
+const Dave = new Enemy("Dave");
+Dave.conversation = "grrr brains";
+Dave.description = "a smelly Zombie";
+Dave.pronoun = "he";
+Dave.weakness = "cheese";
+
+
+// add characters to rooms
+Kitchen.character = Dave;
+
 
 /**
  * Subroutine to display information about the current room
