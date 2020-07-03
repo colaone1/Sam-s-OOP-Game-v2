f5542da33e7d1138da02b3be782eeb390408d155
N Bizzell <bizzbizzell@gmail.com>
2020-07-03 23:33:54 +0100

    revert to not using imports

--- index.js
+++ index.js
@@ -1,104 +1,104 @@
-import Room from './modules/room.js'
-// class Room {
-//   constructor(name) {
-//     this._name = name;
-//     this._description = "";
-//     this._linkedRooms = {};
-//     this._character = "";
-//   }
 
-//   get name() {
-//     return this._name;
-//   }
+class Room {
+  constructor(name) {
+    this._name = name;
+    this._description = "";
+    this._linkedRooms = {};
+    this._character = "";
+  }
 
-//   get description() {
-//     return this._description;
-//   }
+  get name() {
+    return this._name;
+  }
 
-//   get character() {
-//     return this._character
-//   }
+  get description() {
+    return this._description;
+  }
 
-//   set name(value) {
-//     if (value.length < 4) {
-//       alert("Name is too short.");
-//       return;
-//     }
-//     this._name = value;
-//   }
+  get character() {
+    return this._character
+  }
 
-//   set description(value) {
-//     if (value.length < 4) {
-//       alert("description is too short.");
-//       return;
-//     }
-//     this._description = value;
-//   }
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name is too short.");
+      return;
+    }
+    this._name = value;
+  }
 
-//   set character(value) {
-//     this._character = value;
-//   }
+  set description(value) {
+    if (value.length < 4) {
+      alert("description is too short.");
+      return;
+    }
+    this._description = value;
+  }
 
-//   /**
-//    * a method to produce friendly room description
-//    * 
-//    * @returns {string} description of the room
-//    * @author Neil Bizzell
-//    * @version 1.0
-//    */
-//   describe() {
-//     return "Looking around the " + this._name + " you can see " + this._description;
-//   }
+  set character(value) {
+    this._character = value;
+  }
 
-//   /**
-//   * a method to add rooms to link rooms to this one
-//   * it does this by adding them to _linkedRooms
-//   * 
-//   * @param {string} direction the direction the other rooom is from this one
-//   * @param {object} roomToLink the room that is in that direction
-//   * @author Neil Bizzell
-//   * @version 1.0
-//   */
-//   linkRoom(direction, roomToLink) {
-//     this._linkedRooms[direction] = roomToLink;
-//   }
+  /**
+   * a method to produce friendly room description
+   * 
+   * @returns {string} description of the room
+   * @author Neil Bizzell
+   * @version 1.0
+   */
+  describe() {
+    return "Looking around the " + this._name + " you can see " + this._description;
+  }
 
-//   /**
-//    * a method to produce friendly description of linked rooms
-//    * 
-//    * @returns {array} descriptions of what rooms are in which direction
-//    * @author Neil Bizzell
-//    * @version 1.0
-//    */
-//   getDetails() {
-//     const entries = Object.entries(this._linkedRooms);
-//     let details = []
-//     for (const [direction, room] of entries) {
-//       let text = " The " + room._name + " is to the " + direction;
-//       details.push(text);
-//     }
-//     return details;
-//   }
+  /**
+  * a method to add rooms to link rooms to this one
+  * it does this by adding them to _linkedRooms
+  * 
+  * @param {string} direction the direction the other rooom is from this one
+  * @param {object} roomToLink the room that is in that direction
+  * @author Neil Bizzell
+  * @version 1.0
+  */
+  linkRoom(direction, roomToLink) {
+    this._linkedRooms[direction] = roomToLink;
+  }
 
-//   /**
-//    * a method to move the adventurer to a new room
-//    * 
-//    * @param {string} direction the direction in which to move
-//    * @returns {object} the room moved to 
-//    * @author Neil Bizzell
-//    * @version 1.1
-//    */
-//   //method to move to a new room
-//   move(direction) {
-//     if (direction in this._linkedRooms) {
-//       return this._linkedRooms[direction];
-//     } else {
-//       alert("You can't go that way",);
-//       alert(this._name)
-//       return this;
-//     }
-//   }
-// }
+  /**
+   * a method to produce friendly description of linked rooms
+   * 
+   * @returns {array} descriptions of what rooms are in which direction
+   * @author Neil Bizzell
+   * @version 1.0
+   */
+  getDetails() {
+    const entries = Object.entries(this._linkedRooms);
+    let details = []
+    for (const [direction, room] of entries) {
+      let text = " The " + room._name + " is to the " + direction;
+      details.push(text);
+    }
+    return details;
+  }
+
+  /**
+   * a method to move the adventurer to a new room
+   * 
+   * @param {string} direction the direction in which to move
+   * @returns {object} the room moved to 
+   * @author Neil Bizzell
+   * @version 1.1
+   */
+  //method to move to a new room
+  move(direction) {
+    if (direction in this._linkedRooms) {
+      return this._linkedRooms[direction];
+    } else {
+      alert("You can't go that way",);
+      alert(this._name)
+      return this;
+    }
+  }
+}
 
 class Item {
   constructor(name) {
